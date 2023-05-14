4fc323bf1e0a7843ac259c6d29f5ff51a57bf37b
Benoit Donneaux <benoit@leastauthority.com>
2023-05-14 22:33:01 +0200

    Fix image name
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- wormhole/default.nix
+++ wormhole/default.nix
@@ -3,7 +3,7 @@ let
   # Call our lib to build the image
   output = pkgs.callPackage ../nix/lib/docker-image.nix {
     pname = "magic-wormhole";
-    iname = "leastauthority/wormhole";
+    iname = "leastauthority/magic-wormhole";
     config = {
       WorkingDir = "/app";
       EntryPoint = [
