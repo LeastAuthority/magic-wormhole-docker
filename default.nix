fee2bae1c296abfd30de71187a97f4f3ea8a3a62
Benoit Donneaux <benoit@leastauthority.com>
2023-05-14 22:18:34 +0200

    Use a lib and sub-packages
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { pkgs ? import ./nixpkgs.nix { } }:
 {
-  mailbox = pkgs.callPackage ./mailbox/docker-image.nix { };
-  relay = pkgs.callPackage ./relay/docker-image.nix { };
-  wormhole = pkgs.callPackage ./wormhole/docker-image.nix { };
+  # Build our Docker images
+  mailbox = pkgs.callPackage ./mailbox { };
+  relay = pkgs.callPackage ./relay { };
+  wormhole = pkgs.callPackage ./wormhole { };
 }
