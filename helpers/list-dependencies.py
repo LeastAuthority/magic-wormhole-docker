ed8f9ef3a73e00c1ecec463cdac2293ab46d3f0b
Benoit Donneaux <benoit@leastauthority.com>
2023-05-14 23:29:03 +0200

    Use matrix build and improve scan
    Signed-off-by: Benoit Donneaux <benoit@leastauthority.com>

--- helpers/list-dependencies.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-for dir in os.listdir("/nix/store"):
-    print(dir[33:])
-
---
+++ helpers/requirements-from-nix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import re
+
+# Pattern for Python modules
+p = re.compile('python[0-9]+\.[0-9]+-(.+)-([0-9]+(\.[0-9]+){0,2})(-.+)?')
+
+# Iterate through Nix store
+for dir in os.listdir("/nix/store"):
+    # Remove the fix hash part
+    pkg = dir[33:]
+    # Match Python modules
+    m = p.match(pkg)
+    # Print as requirement
+    if m:
+        name = m.group(1)
+        version = m.group(2)
+        suffix = m.group(4) or ""
+        print('{}={}{}'.format(name, version, suffix))
